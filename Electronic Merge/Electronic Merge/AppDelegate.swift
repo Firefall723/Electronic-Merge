6f6bc2b86db3dbce713fb0e6c00d59e9b309d9ac
Michael <michael.kolaski22@d155.org>
2019-04-30 08:53:34 -0500

    First thing

--- Electronic Merge/Electronic Merge/AppDelegate.swift
+++ Electronic Merge/Electronic Merge/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  Electronic Merge
-//
-//  Created by GERARDO LOZOYA on 4/24/19.
-//  Copyright © 2019 CLC.Gerardo. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
