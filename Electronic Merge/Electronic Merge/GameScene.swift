53435cc48f21a7b2c5749b10300cfae9b22af520
GERARDO LOZOYA <gerardo.lozoya20@Matts-MacBook-Pro.local>
2019-05-01 08:41:01 -0500

    created Platform placeholders

--- Electronic Merge/Electronic Merge/GameScene.swift
+++ Electronic Merge/Electronic Merge/GameScene.swift
@@ -1,1 +1,23 @@
-import Foundation
+import SpriteKit
+class GameScene: SKScene {
+    
+    func platforms(positionSetting: Int) {
+        let size = CGSize(width: 50, height: 50)
+        let platform = SKSpriteNode(color: UIColor.brown, size: size)
+        let positionX: [CGFloat] = [0.2, 0.5, 0.8, 0.2, 0.5, 0.8, 0.2, 0.5, 0.8]
+        let positionY: [CGFloat] = [0.5, 0.5, 0.5, 0.6, 0.6, 0.6, 0.7, 0.7, 0.7]
+        
+            let trueX =  ((self.size.width) * positionX[positionSetting])
+            let trueY = ((self.size.height) * positionY[positionSetting])
+            platform.position = CGPoint(x: trueX, y: trueY)
+            platform.name = "platform\(positionSetting + 1)"
+            addChild(platform)
+    }
+    
+    override func didMove(to view: SKView) {
+        backgroundColor = SKColor.red
+        for i in 0...8 {
+        platforms(positionSetting: i)
+        }
+    }
+}
