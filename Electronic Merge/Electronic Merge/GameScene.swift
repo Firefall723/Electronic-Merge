b669691f200044247839cb0c673088af83800cf2
GERARDO LOZOYA <gerardo.lozoya20@Matts-MacBook-Pro.local>
2019-05-03 08:18:23 -0500

    starting the items

--- Electronic Merge/Electronic Merge/GameScene.swift
+++ Electronic Merge/Electronic Merge/GameScene.swift
@@ -1,15 +1,52 @@
 import SpriteKit
 class GameScene: SKScene {
     var time: Int = 10
+    var clock = Timer()
+    let timer = SKLabelNode(text: "")
+    var items: [Int] = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    let platformSize = CGSize(width: 50, height: 50)
     
+    func spawnObject(levelMod: Int, platformPos: CGPoint) {
+        //level mod is unused atm
+        let object = SKSpriteNode(color: .red, size: platformSize)
+        object.position = CGPoint(x: (platformPos.x), y: (platformPos.y + 10))
+    }
+    
+    
+    
+    func updateSpots() {
+        if let i = items.firstIndex(of: 0){
+        items[i] = 1
+            let updatePlatform = childNode(withName: "platform\(i + 1)") as! SKSpriteNode
+        updatePlatform.color = UIColor.green
+            
+        }
+        else {
+            print("no empty spaces")
+        }
+    }
+    
+    func startTimer() {
+        clock = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timeUpdate), userInfo: nil, repeats: true)
+    }
+    
+    @objc func timeUpdate() {
+        if time <= 10 && time >= 1 {
+            time = (time - 1)
+            timer.text = "\(time)"
+        }
+        else {
+            time = 10
+            timer.text = "\(time)"
+            updateSpots()
+        }
+    }
     
     
     func platforms(positionSetting: Int) {
-        let size = CGSize(width: 50, height: 50)
-        let platform = SKSpriteNode(color: UIColor.black, size: size)
+        let platform = SKSpriteNode(color: UIColor.black, size: platformSize)
         let positionX: [CGFloat] = [0.2, 0.5, 0.8, 0.2, 0.5, 0.8, 0.2, 0.5, 0.8]
         let positionY: [CGFloat] = [0.5, 0.5, 0.5, 0.6, 0.6, 0.6, 0.7, 0.7, 0.7]
-        
             let trueX =  ((self.size.width) * positionX[positionSetting])
             let trueY = ((self.size.height) * positionY[positionSetting])
             platform.position = CGPoint(x: trueX, y: trueY)
@@ -18,10 +55,11 @@ class GameScene: SKScene {
     }
     
     func spawnCrate() {
-        
+        //creates the crate also the timer
         let crate = SKSpriteNode(color: SKColor.brown, size: CGSize(width: 100, height: 100))
-        let timer = SKLabelNode(text: "\(time)")
+        timer.text = "10"
         timer.fontColor = SKColor.black
+        timer.fontName = "AvenirNextCondensed-Bold"
         crate.position = CGPoint(x: ((self.size.width) * 0.5) , y: ((self.size.height) * 0.1))
         timer.position = CGPoint(x: (((self.size.width) * 0.5) + 75), y: ((self.size.height) * 0.15))
         addChild(crate)
@@ -32,6 +70,7 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         backgroundColor = SKColor.white
         spawnCrate()
+        startTimer()
         for i in 0...8 {
         platforms(positionSetting: i)
         }
