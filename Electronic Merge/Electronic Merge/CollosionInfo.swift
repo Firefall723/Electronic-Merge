48f50ee09db3fc5257a6267d96d2eee39dc0c7b3
GERARDO LOZOYA <gerardo.lozoya20@Matts-MacBook-Pro.local>
2019-05-07 08:34:10 -0500

    Updated Collosion Info

--- Electronic Merge/Electronic Merge/CollosionInfo.swift
+++ Electronic Merge/Electronic Merge/CollosionInfo.swift
@@ -5,10 +5,32 @@ class CollosionInfo {
     let platformID = 1
     let objectID = 2
     let nilID = 0
+    let platformSize = CGSize(width: 50, height: 50)
+    
+    
+    func spawnObject(levelMod: Int, platformPos: CGPoint, platformSpot: Int) {
+        var object: SKSpriteNode!
+        if levelMod == 1 {
+            object = SKSpriteNode(imageNamed: "Wire")        }
+        else if levelMod == 2 {
+            object = SKSpriteNode(imageNamed: "CircuitBoard")
+        }
+        else {
+            object = SKSpriteNode(color: .red, size: platformSize)
+        }
+        object.size = platformSize
+        object.position = CGPoint(x: (platformPos.x), y: (platformPos.y + 5))
+        object.name = "object\(platformSpot)level\(levelMod)"
+    }
+    
 
     func levelUp(object1: SKSpriteNode, object2: SKSpriteNode, level1: Int, level2: Int, platform1: CGPoint, platform2: CGPoint){
         
         if level1 == level2 {
+            object1.removeFromParent()
+            object2.removeFromParent()
+            let newLevel = (level1 + 1)
+            
             
         }
         else {
