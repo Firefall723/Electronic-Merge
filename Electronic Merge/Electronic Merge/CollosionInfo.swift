b66c63d775b47196568a8366cf7bf775b8cc50db
GERARDO LOZOYA <gerardo.lozoya20@Matts-MacBook-Pro.local>
2019-05-07 08:20:59 -0500

    deleted / updates

--- Electronic Merge/Electronic Merge/CollosionInfo.swift
+++ Electronic Merge/Electronic Merge/CollosionInfo.swift
@@ -1,6 +1,24 @@
 import Foundation
+import SpriteKit
 
 class CollosionInfo {
-    let platformData = 1
+    let platformID = 1
+    let objectID = 2
+    let nilID = 0
+
+    func levelUp(object1: SKSpriteNode, object2: SKSpriteNode, level1: Int, level2: Int, platform1: CGPoint, platform2: CGPoint){
+        
+        if level1 == level2 {
+            
+        }
+        else {
+            object1.position = platform1
+            object2.position = platform2
+        }
+        
+        
+        
+    }
+    
     
 }
